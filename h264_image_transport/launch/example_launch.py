9e2890c21ddcf3ccedc23939e33eb32166acc141
Clyde McQueen <clyde@mcqueen.net>
2021-10-20 07:21:26 -0700

    Fix bug, remove deprecation warnings

--- h264_image_transport/launch/example_launch.py
+++ h264_image_transport/launch/example_launch.py
@@ -37,10 +37,10 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         # Grab h264 packets from a video4linux camera and publish on /image_raw/h264
-        Node(package='h264_image_transport', node_executable='h264_cam_node', output='screen',
-             node_name='h264_cam_node', parameters=[{
-                'input_fn': '/dev/video2',
-                'fps': '30',
+        Node(package='h264_image_transport', executable='h264_cam_node', output='screen',
+             name='h264_cam_node', parameters=[{
+                'input_fn': '/dev/video0',
+                'fps': 30,
                 'size': '800x600',
                 'frame_id': 'camera_frame',
                 'camera_info_path': 'info.ini',
@@ -48,8 +48,8 @@ def generate_launch_description():
 
         # Subscribe to /image_raw/h264, decode, and republish on /repub_raw
         # All remappings are shown for clarity
-        Node(package='image_transport', node_executable='republish', output='screen',
-             node_name='republish_node', arguments=[
+        Node(package='image_transport', executable='republish', output='screen',
+             name='republish_node', arguments=[
                 'h264',  # Input
                 'raw',  # Output
              ], remappings=[
